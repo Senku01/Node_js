e33e79d819013ecd66776d593fc76e62783758da
Dedsec <vigneshprakash2003@gmail.com>
2023-12-05 23:13:07 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+
+
+const os = require('os')
+const path = require('path')
+
+console.log(os.version())
+console.log(os.homedir())
+console.log(os.type())
+
+console.log(__dirname)
+console.log(__filename)
